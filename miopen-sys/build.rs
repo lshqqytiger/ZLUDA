b41641c9e841c59886d5505d1fc84efa9a586ace
Seunghoon Lee <lshqqytiger@naver.com>
2024-03-17 15:57:24 +0900

    github actions miopen fix

--- miopen-sys/build.rs
+++ miopen-sys/build.rs
@@ -4,9 +4,14 @@ use std::{env, path::PathBuf};
 fn main() -> Result<(), VarError> {
     println!("cargo:rustc-link-lib=dylib=MIOpen");
     if cfg!(windows) {
-        let mut path = PathBuf::from(env::var("MIOPEN_PATH")?);
-        path.push("lib");
-        println!("cargo:rustc-link-search=native={}", path.display());
+        let env = env::var("CARGO_CFG_TARGET_ENV")?;
+        if env == "msvc" {
+            let mut path = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+            path.push("lib");
+            println!("cargo:rustc-link-search=native={}", path.display());
+        } else {
+            println!("cargo:rustc-link-search=native=C:\\Windows\\System32");
+        };
     } else {
         println!("cargo:rustc-link-search=native=/opt/rocm/lib/");
     }
