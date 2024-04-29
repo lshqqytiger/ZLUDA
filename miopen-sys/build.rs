7538ae61c6610b71a09805c519cd5e40967a6e08
Seunghoon Lee <lshqqytiger@naver.com>
2024-04-29 22:56:19 +0900

    Remove zluda_dnn remains.

--- miopen-sys/build.rs
+++ miopen-sys/build.rs
@@ -1,19 +1,4 @@
-use std::env::VarError;
-use std::{env, path::PathBuf};
-
-fn main() -> Result<(), VarError> {
+fn main() {
     println!("cargo:rustc-link-lib=dylib=MIOpen");
-    if cfg!(windows) {
-        let env = env::var("CARGO_CFG_TARGET_ENV")?;
-        if env == "msvc" {
-            let mut path = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
-            path.push("lib");
-            println!("cargo:rustc-link-search=native={}", path.display());
-        } else {
-            println!("cargo:rustc-link-search=native=C:\\Windows\\System32");
-        };
-    } else {
-        println!("cargo:rustc-link-search=native=/opt/rocm/lib/");
-    }
-    Ok(())
+    println!("cargo:rustc-link-search=native=/opt/rocm/lib/");
 }
