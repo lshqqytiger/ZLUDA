a5ab2449ae043970ff21e6aea351bad3dcb88588
Seunghoon Lee <lshqqytiger@naver.com>
2025-01-10 00:03:45 +0900

    Bump versions.

--- zluda_inject/src/bin.rs
+++ zluda_inject/src/bin.rs
@@ -83,7 +83,7 @@ pub fn main_impl() -> Result<(), Box<dyn Error>> {
         match argument.to_str() {
             Some(argument) => match argument {
                 "--version" => {
-                    println!("ZLUDA 3.8.5.pre1");
+                    println!("ZLUDA 3.8.6");
                     process::exit(0);
                 }
                 "--" => break,
